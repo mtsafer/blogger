0536e037e25a7260ab28e2b850f630d7dfcd72af
Martin Safer <mtsafer@gmail.com>
2016-06-05 19:38:36 -0700

    Add create and destroy actions to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
 	def index
 		@articles = Article.all
 	end
@@ -7,4 +6,28 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render new_article_path
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id]).destroy
+		redirect_to articles_path
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :body)
+		end
+
 end
